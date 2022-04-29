769c4ac0faf21e7fdb040c92182f54c643bd1123
samantha-hong <96512457+samantha-hong@users.noreply.github.com>
2022-04-29 10:58:33 -0700

    added permutation function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //factorial
 
-document.querySelector('#calc-factorial').addEventListener('click', factorial)
+document.querySelector('#calc-factorial').addEventListener('click', factorial);
 
 const factorialSolution = document.querySelector('#fact-sol');
 const userInput = document.querySelector('#fact-num');
@@ -12,11 +12,9 @@ function factorial(){
     } else if(num === 0){
         factorialSolution.innerText = '1'
     } else{
-        //let fact = 1; 
     let arr = []
     for(let i=1; i <=num; i++){
         arr.push(i);
-       // fact *= i;
     } let fact = arr.reduce((a,b)=>a*b,1)
     factorialSolution.innerText = fact
 }
@@ -24,15 +22,21 @@ function factorial(){
 
 //permutation
 
-// document.querySelector('#permutation').addEventListener('click', permutation)
+document.querySelector('#calc-perm').addEventListener('click', permutation);
 
-// function permutation(){
-//     let n1 = Number(document.querySelector('#n1').value);
-//     let r1 = Number(documnet.querySelector('#r1').value);
-//     if(n1 < r1){
-//         document.querySelector('#perm-sol').innerText = 'Error! n must be larger than r.'
-//     } let perm = 1
-//     for(let i = n1-r1+1; i <= n1; i++){
-//         perm *= i
-//     } document.querySelector('#perm-sol').innerText = perm
-// }
+const permutationSolution = document.querySelector('#perm-sol')
+
+
+function permutation(){
+    let n1 = Number(document.querySelector('#n1').value);
+    let r1 = Number(document.querySelector('#r1').value);
+    if(n1 < r1 || isNaN(n1) || isNaN(r1)){
+        permutationSolution.innerText = 'Error! n must be and integer larger than r.'
+    } else {
+        let arr1 = []
+    for(let i = n1; i > n1-r1; i--){
+        arr1.push(i)
+    }  let perm = arr1.reduce((a,b)=>a*b,1)
+    permutationSolution.innerText = perm
+ } 
+}
